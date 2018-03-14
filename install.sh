36bbda43ccec3d702737580715c6c66fc550c359
Moritz Borcherding <moritz.borcherding@web.de>
2018-03-14 14:17:02 +0100

    mkdir for local bin

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 mkdir -p $HOME/.local/share/applications
 ln -s desktopfiles/restic-cronned.desktop $HOME/.local/share/applications/restic-cronned.desktop
 go build
+mkdir -p $HOME/.local/bin
 ln -s restic-cronned $HOME/.local/bin/restic-cronned
 mkdir -p $HOME/.config/restic-cronned/jobs
