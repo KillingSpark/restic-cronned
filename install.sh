67d873981fe72c69efb4fa473b1fb624f1e1f412
Moritz Borcherding <moritz.borcherding@web.de>
2018-03-14 14:14:13 +0100

    mkdir for desktop files

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p $HOME/.local/share/applications
 ln -s desktopfiles/restic-cronned.desktop $HOME/.local/share/applications/restic-cronned.desktop
 go build
 ln -s restic-cronned $HOME/.local/bin/restic-cronned
